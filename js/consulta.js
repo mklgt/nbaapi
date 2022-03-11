19c4609a01bd2d2d01acd7ff99c708fa6fa65992
Mikel Goicoechea - DAW 2 <goikoetxeamikel85@gmail.com>
2022-03-11 14:06:07 +0100

    Commit

---
+++ js/consulta.js
@@ -0,0 +1,65 @@
+const URL_ROOT = "https://balldontlie.io/api/v1/";
+
+function crearURL(opcion) {
+    return `${URL_ROOT}${opcion}`;
+}
+
+const obtenerInfo = (opcion) => {
+    switch (opcion) {
+        case "games":
+            fetch(crearURL(opcion))
+                .then(response => response.json(), {
+                    method: 'GET',
+                    mode: 'mo-cors',
+                    headers: new Headers({
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Methods': 'GET',
+                        'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+                    })
+                })
+            /*.then(json => {
+                Array.from(json.data).map(resultado => {
+                    let jornada = resultados.date.substring(0, 11);
+                    let temporada = resultados.season;
+                    let local = resultado.home_team.name;
+                    let visitante = resultado.visitor_team.name;
+                    let puntuacionLocal = resultado.home_team_score;
+                    let puntuacionVisitante = resultado.visitor_team_score;
+                    let ganador;
+                    if (puntuacionLocal > puntuacionVisitante) {
+                        ganador = local;
+                    } else {
+                        ganador = visitante;
+                    }
+                    console.log(resultados.date)
+                    tablaResultados.innerHTML += `<tr>
+                    <td headers="jornada" scope="row">${jornada}</td>
+                    <td headers="temporada">${temporada}</td>
+                    <td headers="local">${local}</td>
+                    <td headers="visitante">${visitante}</td>
+                    <td headers="resultadoLocal">${puntuacionLocal}</td>
+                    <td headers="resultadoVisitante">${puntuacionVisitante}</td>
+                    <td headers="equipoGanador" class="font-weight-bold">${ganador}</td>`;
+                })
+            })*/
+
+            break;
+
+        case "teams":
+
+
+
+            break;
+        case "players":
+
+
+
+            break;
+    }
+
+
+
+}
+export {
+    obtenerInfo
+}
---
+++ js/index.js
@@ -0,0 +1,28 @@
+//BOTONES
+let btnResultados = document.getElementById('btnResultados');
+let btnEquipos = document.getElementById('btnEquipos');
+let btnJugadores = document.getElementById('btnJugadores');
+//DIVS DE CONTENIDO
+let resultados = document.getElementById('resultados');
+let equipos = document.getElementById('equipos');
+let jugadores = document.getElementById('jugadores');
+//DONDE PONER EL CONTENIDO
+let tablaResultados = document.getElementById('tablaResultados');
+
+import * as metodos from './consulta.js';
+function inicializar(){
+    /*document.getElementById('opciones').addEventListener('change', function (e) {
+        seleccionObjeto(e.target.value);
+    });*/
+    metodos.obtenerInfo('games')
+}
+window.addEventListener('load', function () {
+    inicializar();
+});
+
+
+
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
