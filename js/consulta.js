0d09b493666c1fe242610251f84c6a2e1e646d33
Mikel Goicoechea <goikoetxeamikel85@gmail.com>
2022-03-13 14:38:03 +0100

    Fetch de resultados

--- js/consulta.js
+++ js/consulta.js
@@ -5,63 +5,66 @@ function crearURL(opcion) {
 }
 
 const obtenerInfo = (opcion) => {
-    fetch(crearURL(opcion), {
-            method: 'GET',
-            mode: 'no-cors',
-            headers: new Headers({
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': '*',
-                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,**;q=0.8',
-                'Host': 'www.balldontlie.io'
-            })
-        })
-        .then(response => response.json())
-        .then(json => {
+    let consulta = fetch(crearURL(opcion));
+    switch (opcion) {
+        case "games":
 
-            switch (opcion) {
-                case "games":
-                    Array.from(json.data).map(resultado => {
-                        let jornada = resultado.date.substring(0, 11);
-                        let temporada = resultado.season;
-                        let local = resultado.home_team.name;
-                        let visitante = resultado.visitor_team.name;
-                        let puntuacionLocal = resultado.home_team_score;
-                        let puntuacionVisitante = resultado.visitor_team_score;
-                        let ganador;
-                        if (puntuacionLocal > puntuacionVisitante) {
-                            ganador = local;
-                        } else {
-                            ganador = visitante;
-                        }
-                        tablaResultados.innerHTML += `<tr>
-                <td headers="jornada" scope="row">${jornada}</td>
-                <td headers="temporada">${temporada}</td>
-                <td headers="local">${local}</td>
-                <td headers="visitante">${visitante}</td>
-                <td headers="resultadoLocal">${puntuacionLocal}</td>
-                <td headers="resultadoVisitante">${puntuacionVisitante}</td>
-                <td headers="equipoGanador" class="font-weight-bold">${ganador}</td>`;
+            
+                consulta.then(response => response.json(), {
+                    method: 'GET',
+                    mode: 'mo-cors',
+                    headers: new Headers({
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Methods': '*',
+                        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+                        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,**;q=0.8',
+                        'Host':'www.balldontlie.io'
                     })
+                })
+            .then(json => {
+                console.log(json)
+                Array.from(json.data).map(resultado => {
+                    let jornada = resultado.date.substring(0, 11);
+                    let temporada = resultado.season;
+                    let local = resultado.home_team.name;
+                    let visitante = resultado.visitor_team.name;
+                    let puntuacionLocal = resultado.home_team_score;
+                    let puntuacionVisitante = resultado.visitor_team_score;
+                    let ganador;
+                    if (puntuacionLocal > puntuacionVisitante) {
+                        ganador = local;
+                    } else {
+                        ganador = visitante;
+                    }
+                    console.log(resultados.date)
+                    tablaResultados.innerHTML += `<tr>
+                    <td headers="jornada" scope="row">${jornada}</td>
+                    <td headers="temporada">${temporada}</td>
+                    <td headers="local">${local}</td>
+                    <td headers="visitante">${visitante}</td>
+                    <td headers="resultadoLocal">${puntuacionLocal}</td>
+                    <td headers="resultadoVisitante">${puntuacionVisitante}</td>
+                    <td headers="equipoGanador" class="font-weight-bold">${ganador}</td>`;
+                })
+            })
+
+            break;
+
+        case "teams":
 
 
-                    break;
 
-                case "teams":
-
-
-                    break;
-                case "players":
+            break;
+        case "players":
 
 
-                    break;
-            }
-        })
+
+            break;
+    }
+
+
+
 }
-
-
-
-
 export {
     obtenerInfo
 }
