bbfa93fcf9b7a3037dc935ac01674e21fa62ad5e
Mikel Goicoechea <goikoetxeamikel85@gmail.com>
2022-03-13 14:25:15 +0100

    Fetch de resultados

--- js/consulta.js
+++ js/consulta.js
@@ -5,7 +5,17 @@ function crearURL(opcion) {
 }
 
 const obtenerInfo = (opcion) => {
-    fetch(crearURL(opcion))
+    fetch(crearURL(opcion), {
+            method: 'GET',
+            mode: 'no-cors',
+            headers: new Headers({
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': '*',
+                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,**;q=0.8',
+                'Host': 'www.balldontlie.io'
+            })
+        })
         .then(response => response.json())
         .then(json => {
 
